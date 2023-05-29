3b4c1b5e2b288fe835c7d49a7bcc6618ddd9d77f
unknown <enio.perfil@gmail.com>
2023-05-29 15:47:53 -0300

    atualização 2  29/05/2023

---
+++ src/app/ajustes/page.tsx
@@ -0,0 +1,10 @@
+import Layout from "@/components/template/Layout"
+
+export default function Ajustes() {
+  return (
+      <Layout titulo="Ajustes & Configurações" 
+        subtitulo="Personalize o Sistema!">
+          <h3>Conteudo!!!</h3>
+      </Layout>
+    )
+}
