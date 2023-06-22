ce14377d1ecb01399bcd028ee81960f60f985ee9
unknown <enio.perfil@gmail.com>
2023-06-22 18:05:53 -0300

    Vercel IV

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,13 +5,10 @@ import 'firebase/firestore'
 
 if (!firebase.apps.length) {
     firebase.initializeApp({
-        apiKey: NEXT_PUBLIC_FIREBASE_API_KEY,
-        authDomain: NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-        projectId: NEXT_PUBLIC_FIREBASE__PROJECT_ID,
-/*        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
         authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
         projectId: process.env.NEXT_PUBLIC_FIREBASE__PROJECT_ID,
-*/
+
     })
 }
 
