401de15bb4ea4134c4de2a66aa1172a28eec75fa
unknown <enio.perfil@gmail.com>
2023-07-13 10:50:33 -0300

    Ajustes Finos II

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Layout from "@/components/template/Layout"
 
 export default function Home() {
   return (
-    <Layout titulo="Como usar Sistema de Cálculo de Execução Penal" subtitulo="Consulte o FAQ e os vídeos a seguir.">
+    <Layout titulo="Como utilizar Sistema de Cálculo de Execução Penal" subtitulo="Aqui você encontrará vídeos e tutoriais sobre como utilizar e obter melhor aproveitamento do sistema de cálculos de execução penal.">
     </Layout>
   )
 }
--- src/components/template/Logo.tsx
+++ src/components/template/Logo.tsx
@@ -1,9 +1,11 @@
-import Localimage from "../../../public/images/LOGO_APP_BRANCO.png"
+//import Localimage from "../../../public/images/LOGO_APP_BRANCO.png"
+import Localimage2 from "../../../public/images/LOGO 2.png"
 import Image from "next/image"
+
 export default function Logo() {
   return (
     <div>
-        <Image src={Localimage}
+        <Image src={Localimage2}
         width={60} height={60}
         alt={'logotipo da empresa'}
      />
