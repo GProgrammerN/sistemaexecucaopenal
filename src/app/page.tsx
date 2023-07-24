c45bea703576bd3a32287de7ffbab50ce6de062e
GProgrammerN <guilherme.programador12@gmail.com>
2023-07-24 14:21:35 -0300

    Visual

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,7 @@ export default function Home() {
     <Layout titulo="Como utilizar Sistema de Cálculo de Execução Penal"
       subtitulo="Aqui você encontrará vídeos e tutoriais sobre como utilizar e obter melhor aproveitamento do sistema de cálculos de execução penal."
       tipoHeight="h-screen">
-      <div className="flex flex-col text-lg text-center underline text-blue-400 decoration-white justify-around flex-wrap
-      sm:flex-row">
+      <div className="flex text-lg text-center underline text-blue-400 decoration-white justify-around flex-wrap">
         <a className="flex sm:hidden" href="https://www.youtube.com/watch?v=5PsZCNBQjLI">Como cadastrar Clientes?</a>
         <a className="flex sm:hidden" href="https://www.youtube.com/watch?v=KAjhaQs_P2k">Como cadastrar Delitos?</a>
         <a className="flex sm:hidden" href="https://www.youtube.com/watch?v=5PsZCNBQjLI">Como cadastrar Remições/Detrações?</a>
