8793e10a30d29477304d165ed71d5e913419b0fd
GProgrammerN <guilherme.programador12@gmail.com>
2023-07-24 09:09:00 -0300

    Videos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,11 @@ export default function Home() {
     <Layout titulo="Como utilizar Sistema de Cálculo de Execução Penal"
       subtitulo="Aqui você encontrará vídeos e tutoriais sobre como utilizar e obter melhor aproveitamento do sistema de cálculos de execução penal."
       tipoHeight="h-screen">
+      <div className="flex justify-around">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/5PsZCNBQjLI" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/r7WEHTNrpA4" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/KAjhaQs_P2k" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
+      </div>
     </Layout>
   )
 }
