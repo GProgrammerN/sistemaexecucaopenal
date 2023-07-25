c4fb6e523fc143974e3ebf1bb2cc31c5fef4d6d9
unknown <enio.perfil@gmail.com>
2023-07-25 11:14:40 -0300

    Versão final

--- src/app/notificacoes/page.tsx
+++ src/app/notificacoes/page.tsx
@@ -9,8 +9,9 @@ export default function Notificacoes() {
     <Layout titulo="Notificações"
       subtitulo="Aqui você irá gerenciar as suas notificações!"
       tipoHeight="h-screen">
-      <Conteudo children='O sistema encontra-se em sua primeira versão, É totalmente funcional e está sempre em constante atualização.' />
-      <Conteudo children='Melhorias e aperfeiçoamentos que houverem serão informados nesta tela, Bem como eventuais manutenções e interrupções se houverem serão previamente informadas aqui.' />
+      <Conteudo children='O sistema encontra-se em sua primeira versão, É totalmente funcional e está sempre em constante atualização, as melhorias e aperfeiçoamentos quando houverem serão informados nesta tela, bem como eventuais manutenções e interrupções se houverem serão previamente informadas aqui.' />
+      <Conteudo children=' ' />
+      <Conteudo children='Quaisquer dúvidas ou solicitações podem ser feitas através do email: enio.perfil@gmail.com, em breve divulgaremos WhatsApp para atendimento. ' />
       <Conteudo children=' ' />
       <Conteudo children='25/07/2023 - o botão gerar pdf agora possibilita 2 tipos de relatórios, a ficha do clientes com as datas das possíveis progressões e também um relatório com os clientes com possíveis progressões e livramento condicional nos próximos 60 dias ou menos bastanto digitar 1 ou 2 e ENTER.' />      
     </Layout>
