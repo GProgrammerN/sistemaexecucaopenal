508abc8aea3afcb26cc3be77ba63b307ae5062c3
unknown <enio.perfil@gmail.com>
2023-11-01 11:18:20 -0300

    IA funcionado estilizando...

--- pages/lib/openai.ts
+++ pages/lib/openai.ts
@@ -1,4 +1,4 @@
-import "dotenv/config"
+//import "dotenv/config"
 import { OpenAI } from "openai"
 
 export const openai = new OpenAI({
--- src/app/ai/page.tsx
+++ src/app/ai/page.tsx
@@ -1,25 +1,50 @@
 // pages/index.js
 
-"use client"
-import { useState } from 'react';
+"use client";
+import { useState, useEffect } from "react";
+import Layout from "@/components/template/Layout";
+import firebase from "../../firebase/config";
+import { TbSelect } from "react-icons/tb"
 
+var prompts = [{}];
+prompts.shift();
+
+type Prompt = {
+  id: string;
+  prompt: string;
+};
 
 export default function Ai() {
-  const [pdfContent, setPdfContent] = useState('');
-  const [promptMessage, setPromptMessage] = useState('')
-  const [aiText, setAiText] = useState('')
- 
-  let prompete = "Como advogado criminalista, seu papel é gerar uma defesa prévia para o acusado, com base na transcrição da denúncia '''{transcription}''', sendo o mais profissional possível e abordando todos as acusações utilizando-se para isso da jurisprudência mais atualizada e também da doutrina com a corrente mais aceita."
+  const [pdfContent, setPdfContent] = useState("");
+  const [promptMessage, setPromptMessage] = useState("");
+  const [aiText, setAiText] = useState("");
+  const db = firebase.firestore();
+  const [prompete, setPrompete] = useState("")
 
-  const handleFileChange = async (event) => {
+  const [prompta, setPrompta] = useState<Prompt[]>();
+
+  useEffect(() => {
+    db.collection("/areas/penal/prompts").get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          var x = doc.data();
+          prompts.push(x);
+        });
+
+        setPrompta(prompts);
+        prompts = [];
+      });
+  }, []);
+
+    const handleFileChange = async (event) => {
     const file = event.target.files[0];
 
     if (file) {
       const formData = new FormData();
-      formData.append('pdf', file);
+      formData.append("pdf", file);
 
-      const response = await fetch('/api/extract-pdf', {
-        method: 'POST',
+      const response = await fetch("/api/extract-pdf", {
+        method: "POST",
         body: formData,
       });
 
@@ -30,34 +55,64 @@ export default function Ai() {
     }
   };
 
-  function mesclar(){
+  function editar(ref: string) {
+    setPrompete(ref)
+  }
+
+  function mesclar() {
     setPromptMessage(prompete.replace("{transcription}", pdfContent));
   }
 
-
   const handleClickButton = async (event) => {
+    const aiResponse = await fetch("/api/generate-ai-completion", {
+      method: "POST",
+      body: JSON.stringify(promptMessage),
+    });
 
-    const aiResponse = await fetch('/api/generate-ai-completion', {
-      method: 'POST',
-      body: JSON.stringify(promptMessage)
-      });
-
-    if(aiResponse.ok) {
+    if (aiResponse.ok) {
       const dataText = await aiResponse.json();
-      setAiText(dataText.text)
+      setAiText(dataText.text);
     }
-
-  } 
+  };
 
   return (
-    <div>
-      <h1>PDF Text Extractor</h1>
-      <input type="file" accept=".pdf" onChange={handleFileChange} />
-      <textarea cols="30" rows="10" value={pdfContent} readOnly></textarea>
-      <button onClick={mesclar}>Mesclar</button>
-      <textarea cols="30" rows="10" value={promptMessage} readOnly></textarea>
-      <button onClick={handleClickButton}>Gerar</button>
-      <textarea cols="30" rows="10" value={aiText} readOnly></textarea>
-    </div>
+    <Layout
+      titulo="Chat GPT 4.0"
+      subtitulo="Aqui você irá utilizar a IA ChatGpt para auxiliá-lo no seu dia a dia!"
+      tipoHeight="h-screen"
+    >
+      <h3>Selecione um arquivo PDF para ser analisado e utilizado pela IA.</h3>
+      <div className="flex flex-col dark:text-black text-white dark:bg-gray-400 dark:placeholder-white">
+        <input
+          type="file"
+          accept=".pdf"
+          onChange={handleFileChange}
+          placeholder="Selecione um arquivo PDF para ser analisado e utilizado pela IA."
+        />
+        <textarea cols="80" rows="8" value={pdfContent} readOnly></textarea>
+        <div className=" bg-blue-400 text-center border-2 h-20 overflow-auto">
+          {prompta?.map((pro) => {
+            return (
+              <div
+                key={pro.id}
+                className="flex justify-start text-sm pl-1 pt-1 items-center"
+              >
+                <a
+                  className="cursor-pointer mr-1 text-green-800" onClick={() => editar(pro.prompt)}
+                >
+                  <TbSelect />
+                </a>
+                <p>{pro.id}</p>
+              </div>
+            );
+          })}
+        </div>
+
+        <button onClick={mesclar}>Mesclar</button>
+        <textarea cols="80" rows="8" value={promptMessage} readOnly></textarea>
+        <button onClick={handleClickButton}>Gerar</button>
+        <textarea cols="80" rows="8" value={aiText} readOnly></textarea>
+      </div>
+    </Layout>
   );
 }
