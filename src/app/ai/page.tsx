eda79d107ba01d0ead14a125c3a6be91c720384b
GProgrammerN <guilherme.programador12@gmail.com>
2023-11-07 16:06:33 -0300

    Layout novo

--- src/app/ai/page.tsx
+++ src/app/ai/page.tsx
@@ -77,39 +77,110 @@ export default function Ai() {
   return (
     <Layout
       titulo="Chat GPT 4.0"
-      subtitulo="Aqui você irá utilizar a IA ChatGpt para auxiliá-lo no seu dia a dia!"
+      subtitulo="Aqui você irá utilizar a IA ChatGPT para auxiliá-lo no seu dia a dia!"
       tipoHeight="h-screen"
     >
-      <h3>Selecione um arquivo PDF para ser analisado e utilizado pela IA.</h3>
-      <div className="flex flex-col dark:text-black text-white dark:bg-gray-400 dark:placeholder-white">
-        <input
-          type="file"
-          accept=".pdf"
-          onChange={handleFileChange}
-          placeholder="Selecione um arquivo PDF para ser analisado e utilizado pela IA."
-        />
-        <textarea cols="80" rows="10" value={pdfContent} readOnly></textarea>
-        <h3>Selecione um dos prompts de IA abaixo clicando no ícone verde.</h3>
-        <div className=" bg-blue-400 text-center border-2 h-24 overflow-auto">
-          {prompta?.map((pro) => {
-            return (
-              <div
-                key={pro.id}
-                className="flex justify-start text-base pl-1 pt-1 items-center"
-              >
-                <a
-                  className="cursor-pointer mr-1 text-green-800"
-                  onClick={() => editar(pro.prompt, pro.id)}
-                >
-                  <TbSelect />
-                </a>
-                <p>{pro.id}</p>
+      <form onSubmit={handleSubmit}>
+
+        <div className="flex flex-col rounded-md
+text-white
+dark:text-black dark:placeholder-white">
+          <div className="flex justify-around bg-gray-900 py-8">
+            <div className="flex flex-col">
+              <h3 className="dark:text-white">Selecione um arquivo PDF para ser analisado e utilizado pela IA.</h3>
+              <input
+                type="file"
+                accept=".pdf"
+                onChange={(event) => {
+                  handleFileChange(event)
+                  handleInputChange(event)
+                }}
+
+                placeholder="Selecione um arquivo PDF para ser analisado e utilizado pela IA."
+                className="rounded-md bg-green-400 w-[90vh]"
+              />
+              <textarea cols="80" rows="10" value={pdfContent} readOnly
+                className="rounded-md"></textarea>
+            </div>
+            <div>
+              <h3 className="dark:text-white">
+                Selecione um dos prompts de IA abaixo clicando no ícone verde.
+              </h3>
+              <div className=" bg-blue-400 text-center w-[90vh] border-2 h-24 overflow-auto">
+                {prompta?.map((pro) => {
+                  return (
+                    <div
+                      key={pro.id}
+                      className="flex justify-start text-base pl-1 pt-1 items-center"
+                    >
+                      <a
+                        className="cursor-pointer mr-1 text-green-800"
+                        onClick={() => editar(pro.prompt, pro.id)}
+                      >
+                        <TbSelect />
+                      </a>
+                      <p>{pro.id}</p>
+                    </div>
+                  );
+                })}
               </div>
-            );
-          })}
-        </div>
-        <div>
-          <form onSubmit={handleSubmit}>
+            </div>
+          </div>
+          <button disabled={isLoading} type="submit" className="w-48 mt-2 mb-2 h-12 rounded-md bg-green-400 self-center
+      hover:bg-green-500">Gerar {descricao}</button>
+          {processando ?
+            <div className='animate-pulse text-center select-none'>
+              <h3>
+                Aguarde processando... (Pode demorar até 3 minutos.)</h3>
+            </div>
+            :
+            <></>}
+          <textarea cols="80" rows="10" value={completion} readOnly
+            className="rounded-md"></textarea>
+        </div >
+      </form >
+    </Layout>
+  )
+  /*
+    <Layout
+      titulo="Chat GPT 4.0"
+      subtitulo="Aqui você irá utilizar a IA ChatGPT para auxiliá-lo no seu dia a dia!"
+      tipoHeight="h-screen"
+    >
+      <form onSubmit={handleSubmit}>
+        <h3>Selecione um arquivo PDF para ser analisado e utilizado pela IA.</h3>
+        <div className="flex flex-col dark:text-black text-white dark:bg-gray-400 dark:placeholder-white">
+          <input
+            type="file"
+            accept=".pdf"
+            value={input}
+            onChange={(event) => {
+              handleFileChange(event)
+              handleInputChange(event)
+            }}
+            placeholder="Selecione um arquivo PDF para ser analisado e utilizado pela IA."
+          />
+          <textarea cols="80" rows="10" value={pdfContent} readOnly></textarea>
+          <h3>Selecione um dos prompts de IA abaixo clicando no ícone verde.</h3>
+          <div className=" bg-blue-400 text-center border-2 h-24 overflow-auto">
+            {prompta?.map((pro) => {
+              return (
+                <div
+                  key={pro.id}
+                  className="flex justify-start text-base pl-1 pt-1 items-center"
+                >
+                  <a
+                    className="cursor-pointer mr-1 text-green-800"
+                    onClick={() => editar(pro.prompt, pro.id)}
+                  >
+                    <TbSelect />
+                  </a>
+                  <p>{pro.id}</p>
+                </div>
+              );
+            })}
+          </div>
+          <div>
             <div>
               <textarea
                 cols="80"
@@ -117,14 +188,15 @@ export default function Ai() {
                 value={completion}
                 readOnly
               ></textarea>
-              <input value={input} onChange={handleInputChange} />
             </div>
             <button disabled={isLoading} type="submit">
               Submit
             </button>
-          </form>
+          </div>
         </div>
-      </div>
+      </form>
     </Layout>
-  );
+          */
+
+
 }
